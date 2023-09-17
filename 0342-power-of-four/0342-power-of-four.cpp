4af023f9a073687740e23232b9363e37c477ce86
sahilhirdhani <118212591+sahilhirdhani@users.noreply.github.com>
2023-09-18 02:42:52 +0530

    Time: 0 ms (100%), Space: 6.7 MB (8.76%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string s="";
+    void fun(int n){
+        if(n==0){
+            return;
+        }
+        fun(n/4);
+        s+=to_string(n%4);
+    }
+    bool check(){
+        for(int i=1;i<s.size();i++){
+            if(s[i]!='0'){
+                return false;
+            }
+        }
+        return true;
+    }
+    bool isPowerOfFour(int n) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        fun(n);
+        if(s[0]=='1' && check()){
+            return true;
+        }
+        return false;
+    }
+};
