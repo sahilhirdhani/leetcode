36f4ad12f26322979e93efb78b062900b7079f66
sahilhirdhani <118212591+sahilhirdhani@users.noreply.github.com>
2023-09-22 10:38:09 +0530

    Time: 0 ms (100%), Space: 6.6 MB (17.6%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0;
+        for(auto x:t){
+            if(x==s[i]){
+                i++;
+                cout<<i<<endl;
+            }
+        }
+        if(i==s.size())
+            return true;
+        return false;
+        
+    }
+};
