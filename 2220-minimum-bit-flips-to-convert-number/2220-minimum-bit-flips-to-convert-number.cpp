6e7bdd1ca6114d5e083205e3217a3280cbf0a186
Sahil Hirdhani <118212591+sahilhirdhani@users.noreply.github.com>
2026-09-17 20:20:47 +0530

    Time: 0 ms (100%), Space: 7.9 MB (60.22%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int num=start^goal;
+        int count=0;
+        for(int i=0;i<30;i++){
+            if(num & (1<<i)){
+                count++;
+            }
+        }
+        return count;
+    }
+};
